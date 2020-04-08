457a1a7d192bdd49c2d5bf6f0f758005b2f334b5
IrsyaAliffio <63340311+IrsyaAliffio@users.noreply.github.com>
2020-04-08 17:08:29 +0700

    hapus alamat, tambah nomor & tambah email

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
 echo "Nama: Irsya Aliffio"; 
 echo "XI RPL 2";
 echo "Asal Sekolah : SMK Taruna Bhakti Depok";
-echo "Alamat Rumah : jl.pekapuran gg.ripan rt 002/005 no.21D";
+echo "Nomor HP : 081548851447";
+echo "Email : irsyaaliffio2806@gmail.com";
 ?>
